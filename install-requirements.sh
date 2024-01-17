f404bbed27a545363463454f3acac570392c62f2
Yvan Pearson <yvan.pearson@gmail.com>
2024-01-17 18:17:02 -0500

    WIP

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+pyenv install
 pip install --upgrade pip
 pip install -r requirements.txt
--- lc1.py
+++ lc1.py
@@ -1,45 +1,34 @@
 import os
-
-# from openai import OpenAI
-
 import openai
-
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 openai.api_key = os.environ['OPENAI_API_KEY']
-# account for deprecation of LLM model
-import datetime
-# Get the current date
-current_date = datetime.datetime.now().date()
 
-# Define the date after which the model should be set to "gpt-3.5-turbo"
-target_date = datetime.date(2024, 6, 12)
-
-# Set the model variable based on the current date
-if current_date > target_date:
-    llm_model = "gpt-3.5-turbo"
-else:
-    # llm_model = "gpt-3.5-turbo-0301"
-    llm_model = "gpt-3.5-turbo"
-
-# from openai import OpenAI
-
-# client = OpenAI()
-
-# completion = client.completions.create(model='curie')
-# print(completion.choices[0].text)
-# print(dict(completion).get('usage'))
-# print(completion.model_dump_json(indent=2))
-
-def get_completion(prompt, model=llm_model):
+def get_completion(prompt, llm_model="gpt-3.5-turbo"):
     client = openai.OpenAI()
     completion = client.chat.completions.create(
-      model="gpt-3.5-turbo",
+      model=llm_model,
       messages=[
-        {"role": "user", "content": "what is 1+1"}
+        {"role": "user", "content": prompt}
       ]
     )
-    print(completion.choices[0].message)
-    return completion.choices[0].message
+    # print(completion)
+    return completion.choices[0].message.content
 
-print(get_completion(""))
+if __name__ == "__main__":
+
+  print(get_completion("what is 2+2"))
+
+  customer_email = """
+Arrr, I be fuming that me blender lid \
+flew off and splattered me kitchen walls \
+with smoothie! And to make matters worse,\
+the warranty don't cover the cost of \
+cleaning up me kitchen. I need yer help \
+right now, matey!
+"""
+
+style = """American English \
+in a calm and respectful tone
+"""
+
